20f47ad92807609c8d63b4fbadbc6a32c9a0119a
rakesh <65104814+rakeshkartikecse@users.noreply.github.com>
2020-06-21 17:07:26 +0530

    index.js changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+var React=require("react");
+var ReactDOM= require("react-dom");
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<h1>Hello World</h1>, where to showit);
