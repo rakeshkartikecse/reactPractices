68ab45d2b57016dfc740b243d61c9e5c4d5b841c
rakesh <65104814+rakeshkartikecse@users.noreply.github.com>
2020-06-22 10:04:34 +0530

    Math.random function is used.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const num = 7;
 ReactDOM.render(
   <div>
-    <h1> My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1>Hello Kartike</h1>
+    <p> Your Lucky number is {Math.floor(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 );
