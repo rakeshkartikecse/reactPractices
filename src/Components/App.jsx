03f28b10bd2eba1e18af0fff4febff66fe2f9302
rakesh <65104814+rakeshkartikecse@users.noreply.github.com>
2020-06-22 18:54:32 +0530

    New folder Components created and all jsx files are put into it.

---
+++ src/Components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "/public/styles.css";
+import Heading from "../Components/Heading";
+import Lists from "../Components/Lists";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Lists />
+      <Lists />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Lists.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Lists() {
+  return (
+    <div>
+      <ul>
+        <li>Beacon</li>
+        <li>Fruits</li>
+
+        <li>Mango</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Lists;
