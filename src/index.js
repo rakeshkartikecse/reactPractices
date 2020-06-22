4efd3dabe6951aa341f66ece213cc8209fa5adcc
rakesh <65104814+rakeshkartikecse@users.noreply.github.com>
2020-06-22 14:32:21 +0530

    variable image after every refresh.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const imga = "https://picsum.photos/500";
 ReactDOM.render(
   <div>
     <h1 className="heading">Created by Kartike</h1>
-    <div>
-      <img
-        className="img-prop"
-        src="https://c.ndtvimg.com/2020-04/720p61v_noodles_625x300_16_April_20.jpg"
-      />
-
-      <img
-        className="img-prop"
-        src="https://www.indianhealthyrecipes.com/wp-content/uploads/2016/05/veg-noodles-recipe-1.jpg"
-      />
-
-      <img
-        className="img-prop"
-        src="https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodlesH2.jpg"
-      />
-    </div>
+    <img src={imga} />
   </div>,
   document.getElementById("root")
 );
