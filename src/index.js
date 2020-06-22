01e722307b805d877dbe4dce5bf53c089c1742e2
rakesh <65104814+rakeshkartikecse@users.noreply.github.com>
2020-06-22 14:22:22 +0530

    Added image tag.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const currentDate = new Date();
-console.log(currentDate);
-const year = currentDate.getFullYear();
 ReactDOM.render(
   <div>
-    <h1>Created by Kartike</h1>
-    <div>Copyrighted {year}</div>
+    <h1 className="heading">Created by Kartike</h1>
+    <div>
+      <img
+        className="img-prop"
+        src="https://c.ndtvimg.com/2020-04/720p61v_noodles_625x300_16_April_20.jpg"
+      />
+
+      <img
+        className="img-prop"
+        src="https://www.indianhealthyrecipes.com/wp-content/uploads/2016/05/veg-noodles-recipe-1.jpg"
+      />
+
+      <img
+        className="img-prop"
+        src="https://www.loveandoliveoil.com/wp-content/uploads/2015/03/soy-sauce-noodlesH2.jpg"
+      />
+    </div>
   </div>,
   document.getElementById("root")
 );
